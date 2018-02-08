57f0e06fb3310ae240daab2b0188008511df00f1
DennyZhang <contact@dennyzhang.com>
2018-02-08 16:01:24 -0600

    show status before deletion

--- terraform_destroy.sh
+++ terraform_destroy.sh
@@ -9,7 +9,7 @@
 ## Description :
 ## --
 ## Created : <2018-02-07>
-## Updated: Time-stamp: <2018-02-08 15:57:51>
+## Updated: Time-stamp: <2018-02-08 16:01:15>
 ##-------------------------------------------------------------------
 set -e
 if [ -z "$do_token" ]; then
@@ -22,5 +22,6 @@ export working_dir="."
 terraform_task_id=${1?}
 cd "$working_dir/$terraform_task_id"
 # TODO: when deleting, we'd better to ask people to provide both hostname and IP as cross reference
+terraform show
 terraform destroy -force --var="do_token=$do_token"
 ## File: terraform_destroy.sh ends
